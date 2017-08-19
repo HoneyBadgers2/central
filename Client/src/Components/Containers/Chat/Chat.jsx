07d969b268bd458dcf6a2d068b6b8b4a1e1a923e
Hack Reactor Student <student@hackreactor.com>
2017-08-19 11:41:50 -0700

    Added drag and drop functionality to chat. User can download dropped files

--- Client/src/Components/Containers/Chat/Chat.jsx
+++ Client/src/Components/Containers/Chat/Chat.jsx
@@ -5,14 +5,22 @@ import NavTask from '../Dashboard/NavTask.jsx';
 import io from 'socket.io-client';
 import {connect} from 'react-redux';
 import $ from 'jquery';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+
+const CLOUDINARY_UPLOAD_PRESET = 'xdbilvan';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dnlk126yf/upload';
+
 
 class Chat extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      messages: [],
+      uploadedFileCloudinaryUrl: {},
+      messages: []
     }
     this.submitMessage = this.submitMessage.bind(this);
+    this.onImageDrop = this.onImageDrop.bind(this);
   }
 
   componentDidMount() {
@@ -36,8 +44,36 @@ class Chat extends Component {
     });
   }
 
-  submitMessage() {
-    const body = $('.chatInput').val();
+   onImageDrop(files) {
+        this.submitMessage(files[0].name);
+        this.handleImageUpload(files[0]);
+    }
+
+  handleImageUpload(file) {
+        let upload = request.post(CLOUDINARY_UPLOAD_URL)
+            .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+            .field('file', file);
+
+        upload.end((err, response) => {
+            if (err) {
+                console.error(err);
+            }
+
+            if (response.body.secure_url !== '') {
+                let temp = {};
+                temp[file.name] = response.body.secure_url;
+                this.setState({
+                    uploadedFileCloudinaryUrl: Object.assign({}, temp, this.state.uploadedFileCloudinaryUrl)      
+                });
+
+                 console.log(response.body);
+            }
+        });
+        
+    }
+
+  submitMessage(draganddrop) {
+    const body = $('.chatInput').val() || draganddrop;
     if (body) {
       const message = {
         body,
@@ -54,9 +90,15 @@ class Chat extends Component {
   render() {
     const messages = this.state.messages.map((message, index) => {
       console.log('message', message)
-      return <ul className="chatMessageContainer" key={index}>
+      return (<ul className="chatMessageContainer" key={index}>
         <div className="chatImageContainer"><img className="chatImageContainerImage" src={message.picture}></img></div>
-        <div className="message"><b>{message.username + ' : '}</b>{message.body}</div></ul>
+        { (message.body in this.state.uploadedFileCloudinaryUrl) ?
+        <div className="message"><b>{message.username + ' : '}</b><a href={this.state.uploadedFileCloudinaryUrl[message.body]} download>{message.body}</a></div> :
+        <div className="message"><b>{message.username + ' : '}</b>{message.body}</div>
+        }
+        </ul>
+        
+        )
     })
     return(
       <div className="dashboard-container">
@@ -70,15 +112,19 @@ class Chat extends Component {
 					<div className="dashboard-title">
 						<h1 className="pull-left">Chat</h1>
 					</div>
-          <div className="chatListContainer">
-            <div className="messagesContainer">
-               {messages}
-            </div>
+          <div className="chatListContainer">    
+                <div className="messagesContainer">
+                  {messages}
+                </div>        
             <div>
               <input className="chatInput" type="text" placeholder='Enter a message...' onKeyPress={(e) => {if(e.key === 'Enter') {this.submitMessage()}}}></input>
               <button className="chatInputButton btn btn-success" onClick={this.submitMessage}>Send Message</button>
+              <Dropzone onDrop={this.onImageDrop}>
+                  <p>Try dropping some files here</p>
+              </Dropzone>
             </div>
           </div>
+          
         </div>
       </div>
     )
